eb459b4ff3779a7d61a1f4e6b428d0de51346933
Loic <45242@etu.he2b.be>
2021-09-21 14:04:16 +0200

    push to work on laptop

--- ftp_rm_scheduling.py
+++ ftp_rm_scheduling.py
@@ -3,6 +3,7 @@ from task_generator import generate_random_tasks
 from numpy import lcm
 from task import Task
 from scheduling import Job
+import numpy as np
 def ftp_rm_schedule(tasks):
     tasks.sort(key=lambda x: x.period)
 
@@ -13,28 +14,42 @@ def get_lcm_tasks_period(tasks):
     return (lcm.reduce(periods))
 #tasks is already sorted
 def is_job_ended(job,task):
-    return job.nb_cpu_unit >= task.wcet
-def is_job_waiting(job, task, instant, jobs_of_same_task):
-    nb_job = len(jobs_of_same_task)
-    v = instant / nb_job
-    w = nb
-    return job.start 
+    return job.nb_cpu_units >= task.wcet
+def is_job_waiting(job, task, instant):
+
+    range_period_start = job.start/task.period
+    range_period_instant = instant / task.period
+    return  not (range_period_instant == range_period_instant)
+    #return job.start 
 def get_ftp_rm_schedule(tasks):
     lcm_tasks = get_lcm_tasks_period(tasks)
     
     #for i in range(len(tasks)):
     schedules =  []
+    for i in range(len(tasks)):
+        schedules.append([])
     last_task_index =0
     for i in range(lcm_tasks):
-
+        print(f"i is {i}")
         for task_index in range(len(tasks)):
-            if (schedules[task_index]["nb_cpu_units"] < tasks[task_index].wcet):
-                if (task_index != last_task_index):
-                    schedules[task_index].append({"start" : i, "nb_cpu_units" : 1})
-                else: 
-                    schedules[task_index][-1]["nb_cpu_units"] +=1
+            if (not schedules[task_index]):
+                schedules[task_index].append(Job(i,1))
+                last_task_index = task_index
+                break
+            job = schedules[task_index][-1]
+            if(task_index != last_task_index and is_job_waiting(job,tasks[task_index], i)):
 
-    
+                if (job.nb_cpu_units < tasks[task_index].wcet):
+                    if (task_index != last_task_index):
+                        schedules[task_index].append(Job(i, 1))
+                    else: 
+                        job.nb_cpu_units +=1
+                    last_task_index = task_index
+                    break
+            if(task_index == last_task_index and not is_job_ended(job, tasks[task_index])):
+                job.nb_cpu_units +=1
+                last_task_index = task_index
+                break
     return schedules
 
 
@@ -56,4 +71,19 @@ def test_ftp_rm_schedule():
     for task in tasks:
         print(task)
 
-test_lcm_tasks_periods()
+def test_ftp_rm_schedule_full():
+    t1 = Task(0, 4 , 20, 20)
+    t2 = Task(0, 2 , 10, 10)
+    t3 = Task(0, 3 , 5, 5)
+    tasks = [t1, t2, t3]
+    ftp_rm_schedule(tasks)
+    for task in tasks:
+        print(task)
+    schedules = get_ftp_rm_schedule(tasks)
+    for index, schedule in enumerate(schedules):
+        print("Task : "+str(index))
+        for job in schedule:
+            print(str(job))
+        #print(str(schedule))
+#test_lcm_tasks_periods()
+test_ftp_rm_schedule_full()
--- scheduling.py
+++ scheduling.py
@@ -6,3 +6,6 @@ class Job:
     def __init__(self, start=0, nb_cpu_units=0):
         self.start = start
         self.nb_cpu_units = nb_cpu_units
+
+    def __str__(self):
+        return f"{self.start} {self.nb_cpu_units}"
