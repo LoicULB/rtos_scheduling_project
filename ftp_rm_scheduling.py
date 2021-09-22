1d1f05fd5233f2190d69809b59beaa5699fd804c
Loic <45242@etu.he2b.be>
2021-09-22 15:06:24 +0200

    nothing changed

--- ftp_rm_scheduling.py
+++ ftp_rm_scheduling.py
@@ -1,8 +1,8 @@
 
-from task_generator import generate_random_tasks
+from .task_generator import generate_random_tasks
 from numpy import lcm
-from task import Task
-from scheduling import Job
+from .task import Task
+from .scheduling import Job
 import numpy as np
 def ftp_rm_schedule(tasks):
     tasks.sort(key=lambda x: x.period)
--- tests/test_scheduling_execution.py
+++ tests/test_scheduling_execution.py
@@ -1,9 +1,11 @@
 from ..ftp_rm_scheduling import get_ftp_rm_schedule
-from task import Task
+from ..task import Task
 
 def test_execution_of_single_task():
     t = Task(0, 3, 5, 5)
     tasks = [t]
     schedules = get_ftp_rm_schedule(tasks)
+    print(schedules[0])
+
     assert (len(schedules)) == 1
     
