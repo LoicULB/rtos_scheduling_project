5807e44c96892f4add5f6fdddfbdb97a7b556b64
SachaKeserovic <keserovicsacha@yahoo.fr>
2021-10-02 17:54:16 +0200

    Progress in Audsley Algorithm

--- audsley.py
+++ audsley.py
@@ -1,7 +1,9 @@
-from task import Task
-from scheduling import SystemScheduling
 from exceptions import DeadlineMissedException
-def is_task_lowest_priority_viable(task_index : int, tasks : List[Task] ):
+from scheduling import SystemScheduling
+from task import Task
+
+
+def is_task_lowest_priority_viable(task_index, tasks):
     """Checks if the tasks of the pointed by the task index in the tasks list is lower priority viable
 
     Args:
@@ -10,39 +12,51 @@ def is_task_lowest_priority_viable(task_index : int, tasks : List[Task] ):
     Return:
         boolean : true if the task is lowest priority viable
     """
-    new_tasks_set= tasks.copy()
-    #assign the lowest priority to the task
-    task =new_tasks_set.pop(task_index)
-    task.is_hard=True
+    new_tasks_set = tasks.copy()
+    # assign the lowest priority to the task
+    task = new_tasks_set.pop(task_index)
+    task.is_hard = True
     new_tasks_set.append(task)
 
-    #it justs works
-    sys_sched= SystemScheduling(new_tasks_set)
-    try :
+    # it justs works
+    sys_sched = SystemScheduling(new_tasks_set)
+    try:
         sys_sched.execute_FTP_schedule()
+        task.is_hard = False
         return True
     except DeadlineMissedException:
+        task.is_hard = False
         return False
 
+
 def make_all_tasks_soft(tasks):
+    """
+    Make all tasks from the task set soft
+    :param tasks: the task set
+    :return: none
+    """
     for task in tasks:
-        task.is_hard=False
-def audlsey(tasks: List[Task], leftover_tasks : List[Task]):
+        task.is_hard = False
+
+
+def audsley_recur(tasks, leftover_tasks):
     """Find a FTP schedulable priority assignement for task set "tasks".
 
     Args:
         tasks (List[Task]): tasks set of which we will assign a FTP schedulable priority ordering.
         This list will be sorted at the end of the algorithm.
     """
-    if(tasks):
-        for i, task in enumerate(tasks):
-            if is_task_lowest_priority_viable(i,tasks):
-                audlsey(tasks, tasks.copy().remove(i))
+    if (leftover_tasks):
+        for i, task in enumerate(leftover_tasks):
+            if is_task_lowest_priority_viable(i, leftover_tasks):
                 tasks.pop(i)
-                tasks.append(task)
-                 
-                
-                #break
+                tasks.insert(len(leftover_tasks) - 1, task)
+                leftover_tasks.pop(i)
+
+                return audsley_recur(tasks, leftover_tasks)
+
         return False
-    return False   
+
+    return True
+
 
